de828457736cd883adcc23aea8322b4f4d3a0b33
Talhamahmood23 <directoptimize@gmail.com>
2024-09-04 16:06:11 +0500

    Initial Commit

---
+++ Vendorbaz/VendorbazApp.swift
@@ -0,0 +1,17 @@
+//
+//  VendorbazApp.swift
+//  Vendorbaz
+//
+//  Created by Atlas on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct VendorbazApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
